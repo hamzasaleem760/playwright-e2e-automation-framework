87f90aa8d6662add862def2d15ab251a13b8b8dd
Hamza Bin Saleem <128089346+hamzasaleem760@users.noreply.github.com>
2026-06-17 03:36:36 +0200

    implemented modular login page object component

---
+++ pages/LoginPage.ts
@@ -0,0 +1,31 @@
+import { Locator, Page } from '@playwright/test';
+
+export class LoginPage {
+  private readonly page: Page;
+  private readonly usernameInput: Locator;
+  private readonly passwordInput: Locator;
+  private readonly loginButton: Locator;
+  private readonly errorMessage: Locator;
+
+  constructor(page: Page) {
+    this.page = page;
+    this.usernameInput = page.locator('[data-test="username"]');
+    this.passwordInput = page.locator('[data-test="password"]');
+    this.loginButton = page.locator('[data-test="login-button"]');
+    this.errorMessage = page.locator('[data-test="error"]');
+  }
+
+  async navigate() {
+    await this.page.goto('https://www.saucedemo.com/');
+  }
+
+  async login(username: string, pass: string) {
+    await this.usernameInput.fill(username);
+    await this.passwordInput.fill(pass);
+    await this.loginButton.click();
+  }
+
+  async getErrorMessage() {
+    return this.errorMessage;
+  }
+}
